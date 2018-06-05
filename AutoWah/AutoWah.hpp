536098c8bbbc3a54d176f3fa6624547cfc224178
Mathias <mathiasfriis1995@hotmail.com>
2018-06-05 17:04:30 +0200

    Uploader SVF og AutoWah

---
+++ AutoWah/AutoWah.hpp
@@ -0,0 +1,95 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ 
+ 
+ LICENSE:
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
+/* created by the OWL team 2013 */
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef ___AutoWahPatch_hpp__
+#define __AutoWahPatch_hpp__
+
+#include "CircularBuffer.hpp"
+#include "lfo.hpp"
+#include "SVF.hpp"
+
+#define FLANGER_BUFFER_SIZE 1024
+#define CUTOFF_MIN 0
+#define CUTOFF_SCALER 8000
+#define Q_SCALER 30
+#define RATE_SCALER 100
+#define DEPTH_SCALER 8000
+
+class AutoWahPatch : public Patch {
+private:
+    float rate, depth, Q, fc;
+    lfo_mode mode;
+    LFO lfo;
+    StateVariableFilter filter;
+
+    
+public:
+  AutoWahPatch(){
+    AudioBuffer* buffer = createMemoryBuffer(1, FLANGER_BUFFER_SIZE);
+    registerParameter(PARAMETER_A, "Rate");
+    registerParameter(PARAMETER_B, "Depth");
+    registerParameter(PARAMETER_C, "Q");
+    registerParameter(PARAMETER_D, "Cutoff");
+    lfo.initLFO();
+    float fs = getSampleRate();
+    lfo.setSampleRate(fs);
+    filter.initFilter(fs);
+
+    lfo.setWaveshape(sine);
+    filter.setFilterType(LOW_PASS);
+  }
+ 
+
+  void processAudio(AudioBuffer &buffer){
+    int size = buffer.getSize();
+    unsigned int delaySamples;
+      
+    rate     = (getParameterValue(PARAMETER_A)*RATE_SCALER);
+    depth    = getParameterValue(PARAMETER_B)*DEPTH_SCALER;
+    Q = getParameterValue(PARAMETER_C)*Q_SCALER; // so we keep a -3dB summation of the delayed signal
+    fc= getParameterValue(PARAMETER_C)*100+CUTOFF_MIN;
+    
+    filter.setQfactor(Q);
+    filter.setCutoff(fc+lfo.get_LFO_value*depth);
+    
+    lfo.setFrequency(rate);
+      
+    for (int ch = 0; ch<buffer.getChannels(); ++ch) {
+        for (int i = 0 ; i < size; i++) {
+            float* buf = buffer.getSamples(ch);
+            lfo.updateLFO_value();
+
+
+
+            float dry = buf[i]*(1-1);
+            float wet = doFiltering((int16_t)buf[i]);;
+            buf[i] = dry+wet;
+        }
+    }
+  }
+    
+};
+
+
+#endif /* __FlangerPatch_hpp__ */
---
+++ Util/State Variable Filter/SVF.hpp
@@ -0,0 +1,83 @@
+#ifndef SVT_HPP_
+#define SVT_HPP_
+#include <stdint.h>
+#include <math.h>
+
+typedef enum
+{
+	LOW_PASS = 0,
+	BAND_PASS = 1,
+	HIGH_PASS = 2
+} SVF_FILTER_TYPE;
+
+
+class StateVariableFilter
+{
+private:
+	float hpf = 0;
+	float bpf = 0;
+	float lpf = 0;
+	float Q = 0;
+	uint16_t fc = 0;
+	float sampleRate;
+	SVF_FILTER_TYPE type = LOW_PASS;
+public:
+	void setFilterType(SVF_FILTER_TYPE filter_type);
+	void setCutoff(float frequency);
+	void setQfactor(float value);
+	void initFilter(float fs);
+	int16_t doFiltering(int16_t x);
+}
+
+//Initialize filter
+void StateVariableFilter::initFilter(float fs)
+{
+	sampleRate=fs;
+}
+
+// Set filter type
+void StateVariableFilter::setFilterType(SVF_FILTER_TYPE filter_type)
+{
+	type = filter_type;
+}
+
+// Set Q-factor
+void StateVariableFilter::setQfactor(float value)
+{
+	Q = value + 1;
+}
+
+// Set cut off frequency
+void StateVariableFilter::setCutoff(uint16_t freq)
+{
+	fc = freq;
+}
+
+// SVF pass
+void StateVariableFilter::SVFpass(int16_t x)
+{
+    hpf = q * x - lpf - q * bpf;
+    bpf = hpf * f + bpf;
+    lpf = bpf * f + lpf;
+}
+
+// State variable filter
+int16_t StateVariableFilter::doFiltering(int16_t x)
+{
+    float q = 1 / Q;
+    float f = sin((3.141593 * fc)/SAMPLE_FREQUENCY);
+    if(f >= 0.5) f = 0.5;
+
+    // Run two passes
+    SVFpass(x);
+    SVFpass(x);
+
+    // Return selected filter outputs
+    if (type == LOW_PASS) return lpf
+    if (type == HIGH_PASS) return hpf
+    if (type == BAND_PASS) return bpf
+    return 0;
+}
+
+
+#endif
