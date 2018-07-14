7aa105914d31cc9c893fa13f96276c9d707240c6
Mathias <mathiasfriis1995@hotmail.com>
2018-07-14 15:41:52 +0200

    SVF seems to work.

--- Util/State Variable Filter/SVF.hpp
+++ Util/State Variable Filter/SVF.hpp
@@ -75,7 +75,6 @@ void StateVariableFilter::SVFpass(float x)
 float StateVariableFilter::doFiltering(float x)
 {
     q = 1 / Q;
-    q=0.99;
     //f = sin((3.141593 * fc/2));
     //f=2*sin(3.141593*fc/sampleRate);
     f=2*(3.141593*fc/sampleRate);
