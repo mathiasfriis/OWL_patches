b67993d6fecbfaa9827797431330fd88abb4c495
Mathias <mathiasfriis1995@hotmail.com>
2018-07-14 13:22:23 +0200

    a

--- Util/State Variable Filter/SVF.hpp
+++ Util/State Variable Filter/SVF.hpp
@@ -71,7 +71,7 @@ void StateVariableFilter::SVFpass(float x)
 float StateVariableFilter::doFiltering(float x)
 {
     //q = 1 / Q;
-    q=0.1;
+    q=5;
     //f = sin((3.141593 * fc/2));
     f=2*sin(3.141593*fc/sampleRate);
 
