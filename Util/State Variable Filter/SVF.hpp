031e641e3e84e19f265c32ff480531f82f6bcf93
Mathias <mathiasfriis1995@hotmail.com>
2018-07-14 16:25:04 +0200

    Restraints to FC
    ++

--- Util/State Variable Filter/SVF.hpp
+++ Util/State Variable Filter/SVF.hpp
@@ -62,7 +62,7 @@ void StateVariableFilter::setQfactor(float value)
 void StateVariableFilter::setCutoff(float freq)
 {
 	fc = freq;
-	/*
+	
 	if(fc<0)
 	{
 		fc=0;
@@ -71,7 +71,7 @@ void StateVariableFilter::setCutoff(float freq)
 	{
 		fc=sampleRate/2;
 	}
-	*/
+	
 }
 
 // SVF pass
