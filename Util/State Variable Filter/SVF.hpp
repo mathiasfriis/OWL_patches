c5cfda216c7d970e1466243d84ded09ffdeb1a0b
Mathias <mathiasfriis1995@hotmail.com>
2018-07-14 13:24:45 +0200

    a

--- Util/State Variable Filter/SVF.hpp
+++ Util/State Variable Filter/SVF.hpp
@@ -84,7 +84,7 @@ float StateVariableFilter::doFiltering(float x)
     //SVFpass(x);
 
     // Return selected filter outputs
-    return bpf;
+    return hpf;
     if (type == LOW_PASS) return lpf;
     if (type == HIGH_PASS) return hpf;
     if (type == BAND_PASS) return bpf;
