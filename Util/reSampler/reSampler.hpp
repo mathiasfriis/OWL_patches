4f373f84e7eb6d4d8d3802c6b83f8e5b26b23112
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-09-12 15:44:37 +0200

    a

--- Util/reSampler/reSampler.hpp
+++ Util/reSampler/reSampler.hpp
@@ -15,7 +15,7 @@ private:
 public:
 	void initDownSampler();
 	void downSample(AudioBuffer &inputBuffer, AudioBuffer &outputBuffer, int decimationRate);
-	void upSample(AudioBuffer &inputbuffer, float &outputBuffer, int interpolationRate);
+	void upSample(AudioBuffer &inputbuffer, float &outputBuffer[], int interpolationRate);
 	void reSample(AudioBuffer &inputbuffer, AudioBuffer &outputBuffer, float multiRate, float multiRateMargin);
 	void setInputSampleRate(float fs);
 	void setOutputSampleRate(float fs);
@@ -55,7 +55,7 @@ void reSampler::downSample(AudioBuffer &inputBuffer, AudioBuffer &outputBuffer, 
 }
 
 //Unknown if this works!
-void reSampler::upSample(AudioBuffer &inputBuffer, float &outputBuffer, int interpolationRate)
+void reSampler::upSample(AudioBuffer &inputBuffer, float &outputBuffer[], int interpolationRate)
 {
 	//get size of inputbuffer - NOTE: SIZE OF BUFFERS MUST MATCH!
 	int inputBufferSize = inputBuffer.getSize();
@@ -98,7 +98,7 @@ void reSampler::reSample(AudioBuffer &inputBuffer, AudioBuffer &outputBuffer, fl
 	//*InterpolatedSignalBuffer = malloc(sizeof(float)*inputBufferSize*L);
 
 	//Interpolate signal by a rate of L and save in new buffer
-	upSample(inputBuffer, *InterpolatedSignalBuffer, L);
+	upSample(inputBuffer, InterpolatedSignalBuffer, L);
 
 	//Downsample interpolated signal by a rate of M and save in outputBuffer.
 
