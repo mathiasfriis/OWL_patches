eba3b0940b6e17f9b5615a1c28ede90a5d7550cf
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-09-17 10:09:06 +0200

    a

--- Util/reSampler/reSampler.hpp
+++ Util/reSampler/reSampler.hpp
@@ -127,7 +127,7 @@ void reSampler::reSample(AudioBuffer &inputBuffer, AudioBuffer &outputBuffer, fl
 		//buf[i]=getDecimalSampleWithInterpolation(InterpolatedSignalBuffer,i*achievedMultiRate);
 	}
 
-	free (*InterpolatedSignalBuffer);
+	free (InterpolatedSignalBuffer);
 
 	return;
     }
