172441a78c5c99e0bd4d6ea01aab2f970da27c9f
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-10-11 12:01:37 +0200

    a

--- Delay/DelayTestPatch.hpp
+++ Delay/DelayTestPatch.hpp
@@ -78,7 +78,7 @@ public:
         float* buf = buffer.getSamples(ch);
         for (int i = 0 ; i < size; i++) {
             float dry = buf[i]*(1-depth); //Get dry signal
-            float wet = y->read(delaySamples); //Get wet signal
+            float wet = (y->read(delaySamples))*depth; //Get wet signal
             buf[i] = dry+wet;
             y->write(buf[i]*feedback); //Write to y-buffer
         }
