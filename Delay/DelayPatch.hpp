6b7d81d4c565c21138f9bc552c462897e018dff6
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-10-11 10:15:02 +0200

    Påbegynder DelayPatch

---
+++ Delay/DelayPatch.hpp
@@ -0,0 +1,77 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ 
+ 
+ LICENSE:
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
+/* created by the OWL team 2013 */
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef ___DelayPatch_hpp__
+#define __DelayPatch_hpp__
+
+#include "CircularBuffer.hpp"
+#include "lfo.hpp"
+
+#define MAX_DELAY_MS 3000
+
+class TremoloPatch : public Patch {
+private:
+    CircularBuffer x, y;
+    float delay_ms;
+    float feedback;
+
+public:
+  TremoloPatch(){
+    float fs = getSampleRate();
+    AudioBuffer* buffer = createMemoryBuffer(1, FLANGER_BUFFER_SIZE);
+    x = CircularBuffer::create(MAX_DELAY_MS*fs);
+    y = CircularBuffer::create(MAX_DELAY_MS*fs);
+    registerParameter(PARAMETER_A, "Delay");
+    registerParameter(PARAMETER_B, "Feedback");
+    registerParameter(PARAMETER_D, "Depth");
+  }
+ 
+
+  void processAudio(AudioBuffer &buffer){
+    int size = buffer.getSize();
+      
+    //Read knobs
+    delay_ms     = getParameterValue(PARAMETER_A)*MAX_DELAY_MS;
+    feedback = getParameterValue(PARAMETER_B);
+    depth    = getParameterValue(PARAMETER_D);
+
+    //Calculate delay in samples
+    unsigned int delaySamples = delay_ms*fs/1000;
+
+    for (int ch = 0; ch<buffer.getChannels(); ++ch) {
+        
+        float* buf = buffer.getSamples(ch);
+        for (int i = 0 ; i < size; i++) {
+            float dry = buf[i]*(1-depth); //Get dry signal
+            float wet = y->read(delaySamples); //Get wet signal
+            buf[i] = dry+wet;
+            y->write(delaySamples*feedback); //Write to write to y-buffer
+        }
+    }
+  }
+    
+};
+
+
+#endif /* __FlangerPatch_hpp__ */
