1e247f3ae3604ee6d34c88097c3d300b90c8713e
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-10-26 15:43:43 +0200

    Update DecimateOnDemand/DecimateOnDemandPatch.hpp

--- DecimateOnDemand/DecimateOnDemandPatch.hpp
+++ DecimateOnDemand/DecimateOnDemandPatch.hpp
@@ -107,7 +107,7 @@ public:
     {
     	//calculate relative sample rate
    		//relativeSampleRate = 1 - FS_OFFSET - eg.getValue()*depth*(1-FS_OFFSET);
-   		relativeSampleRate = 1 - FS_OFFSET + pow(1/(eg.getValue()+1),5)*depth*(1-FS_OFFSET); //Gives an exponential-like movement, so that movement around lower sample freqs are more fine, and more course in higher.
+   		relativeSampleRate = pow(1/(eg.getValue()+1),5) * depth * (1-FS_OFFSET); //Gives an exponential-like movement, so that movement around lower sample freqs are more fine, and more course in higher.
     }
     
      
